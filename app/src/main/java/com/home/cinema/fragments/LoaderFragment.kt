eeadf6d74feb2d6a8774a1d9229084a9f6780afc
root <poddubniysergey1981@gmail.com>
2023-01-28 11:42:18 +0300

    Init

---
+++ app/src/main/java/com/home/cinema/fragments/LoaderFragment.kt
@@ -0,0 +1,22 @@
+package com.home.cinema.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.home.cinema.databinding.FragmentLoaderBinding
+
+class LoaderFragment : Fragment() {
+
+    private var binding: FragmentLoaderBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLoaderBinding.inflate(inflater, container, false)
+        return binding!!.root
+    }
+}
---
+++ app/src/main/java/com/home/cinema/fragments/OnBoardingFragment.kt
@@ -0,0 +1,6 @@
+package com.home.cinema.fragments
+
+import androidx.fragment.app.Fragment
+
+class OnBoardingFragment: Fragment() {
+}
