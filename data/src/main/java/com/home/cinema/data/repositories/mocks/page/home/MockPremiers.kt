a40719dda805a90e9a677f4abc95831c6c349eb9
PjddubniySerg <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>
2023-02-10 00:41:38 +0300

    Home page: update layouts.

---
+++ data/src/main/java/com/home/cinema/data/repositories/mocks/page/home/MockPremiers.kt
@@ -0,0 +1,338 @@
+package com.home.cinema.data.repositories.mocks.page.home
+
+import com.home.cinema.domain.models.entities.page.home.GenreString
+import com.home.cinema.domain.models.entities.page.home.Movie
+
+class MockPremiers {
+
+    val premiers = listOf<Movie>(
+
+        Premier(
+            8403,
+            "https://kinopoiskapiunofficial.tech/images/posters/kp_small/8403.jpg",
+            "Небо над Берлином",
+            "Der Himmel über Berlin",
+            listOf(
+                MockGenresStr("фэнтези"),
+                MockGenresStr("драма"),
+                MockGenresStr("мелодрама")
+            ),
+            7.258,
+            "2023-02-15",
+            true
+        ),
+        Premier(
+            5045951,
+            "https://kinopoiskapiunofficial.tech/images/posters/kp_small/5045951.jpg",
+            "О чем говорят мужчины. Простые удовольствия",
+            "",
+            listOf(
+                MockGenresStr("комедия")
+            ),
+            null,
+            "2023-02-15",
+            true
+        ),
+        Premier(
+            4673172,
+            "https://kinopoiskapiunofficial.tech/images/posters/kp_small/4673172.jpg",
+            "Хищники",
+            "As bestas",
+            listOf(
+                MockGenresStr("триллер"),
+                MockGenresStr("драма")
+            ),
+            7.258,
+            "2023-02-15",
+            false
+        ),
+        Premier(
+            4,
+            "https://kinopoiskapiunofficial.tech/images/posters/kp_small/8403.jpg",
+            "Небо над Берлином",
+            "Der Himmel über Berlin",
+            listOf(
+                MockGenresStr("фэнтези"),
+                MockGenresStr("драма"),
+                MockGenresStr("мелодрама")
+            ),
+            7.258,
+            "2023-02-15",
+            true
+        ),
+        Premier(
+            5,
+            "https://kinopoiskapiunofficial.tech/images/posters/kp_small/5045951.jpg",
+            "О чем говорят мужчины. Простые удовольствия",
+            "",
+            listOf(
+                MockGenresStr("комедия")
+            ),
+            null,
+            "2023-02-15",
+            true
+        ),
+        Premier(
+            6,
+            "https://kinopoiskapiunofficial.tech/images/posters/kp_small/4673172.jpg",
+            "Хищники",
+            "As bestas",
+            listOf(
+                MockGenresStr("триллер"),
+                MockGenresStr("драма")
+            ),
+            7.258,
+            "2023-02-15",
+            false
+        ),
+        Premier(
+            7,
+            "https://kinopoiskapiunofficial.tech/images/posters/kp_small/8403.jpg",
+            "Небо над Берлином",
+            "Der Himmel über Berlin",
+            listOf(
+                MockGenresStr("фэнтези"),
+                MockGenresStr("драма"),
+                MockGenresStr("мелодрама")
+            ),
+            7.258,
+            "2023-02-15",
+            true
+        ),
+        Premier(
+            8,
+            "https://kinopoiskapiunofficial.tech/images/posters/kp_small/5045951.jpg",
+            "О чем говорят мужчины. Простые удовольствия",
+            "",
+            listOf(
+                MockGenresStr("комедия")
+            ),
+            null,
+            "2023-02-15",
+            true
+        ),
+        Premier(
+            9,
+            "https://kinopoiskapiunofficial.tech/images/posters/kp_small/4673172.jpg",
+            "Хищники",
+            "As bestas",
+            listOf(
+                MockGenresStr("триллер"),
+                MockGenresStr("драма")
+            ),
+            7.258,
+            "2023-02-15",
+            false
+        ),
+        Premier(
+            10,
+            "https://kinopoiskapiunofficial.tech/images/posters/kp_small/8403.jpg",
+            "Небо над Берлином",
+            "Der Himmel über Berlin",
+            listOf(
+                MockGenresStr("фэнтези"),
+                MockGenresStr("драма"),
+                MockGenresStr("мелодрама")
+            ),
+            7.258,
+            "2023-02-15",
+            true
+        ),
+        Premier(
+            11,
+            "https://kinopoiskapiunofficial.tech/images/posters/kp_small/5045951.jpg",
+            "О чем говорят мужчины. Простые удовольствия",
+            "",
+            listOf(
+                MockGenresStr("комедия")
+            ),
+            null,
+            "2023-02-15",
+            true
+        ),
+        Premier(
+            12,
+            "https://kinopoiskapiunofficial.tech/images/posters/kp_small/4673172.jpg",
+            "Хищники",
+            "As bestas",
+            listOf(
+                MockGenresStr("триллер"),
+                MockGenresStr("драма")
+            ),
+            7.258,
+            "2023-02-15",
+            false
+        ),
+
+
+        Premier(
+            13,
+            "https://kinopoiskapiunofficial.tech/images/posters/kp_small/8403.jpg",
+            "Небо над Берлином",
+            "Der Himmel über Berlin",
+            listOf(
+                MockGenresStr("фэнтези"),
+                MockGenresStr("драма"),
+                MockGenresStr("мелодрама")
+            ),
+            7.258,
+            "2023-02-15",
+            true
+        ),
+        Premier(
+            14,
+            "https://kinopoiskapiunofficial.tech/images/posters/kp_small/5045951.jpg",
+            "О чем говорят мужчины. Простые удовольствия",
+            "",
+            listOf(
+                MockGenresStr("комедия")
+            ),
+            null,
+            "2023-02-15",
+            true
+        ),
+        Premier(
+            15,
+            "https://kinopoiskapiunofficial.tech/images/posters/kp_small/4673172.jpg",
+            "Хищники",
+            "As bestas",
+            listOf(
+                MockGenresStr("триллер"),
+                MockGenresStr("драма")
+            ),
+            7.258,
+            "2023-02-15",
+            false
+        ),
+        Premier(
+            16,
+            "https://kinopoiskapiunofficial.tech/images/posters/kp_small/8403.jpg",
+            "Небо над Берлином",
+            "Der Himmel über Berlin",
+            listOf(
+                MockGenresStr("фэнтези"),
+                MockGenresStr("драма"),
+                MockGenresStr("мелодрама")
+            ),
+            7.258,
+            "2023-02-15",
+            true
+        ),
+        Premier(
+            17,
+            "https://kinopoiskapiunofficial.tech/images/posters/kp_small/5045951.jpg",
+            "О чем говорят мужчины. Простые удовольствия",
+            "",
+            listOf(
+                MockGenresStr("комедия")
+            ),
+            null,
+            "2023-02-15",
+            true
+        ),
+        Premier(
+            18,
+            "https://kinopoiskapiunofficial.tech/images/posters/kp_small/4673172.jpg",
+            "Хищники",
+            "As bestas",
+            listOf(
+                MockGenresStr("триллер"),
+                MockGenresStr("драма")
+            ),
+            7.258,
+            "2023-02-15",
+            false
+        ),
+        Premier(
+            19,
+            "https://kinopoiskapiunofficial.tech/images/posters/kp_small/8403.jpg",
+            "Небо над Берлином",
+            "Der Himmel über Berlin",
+            listOf(
+                MockGenresStr("фэнтези"),
+                MockGenresStr("драма"),
+                MockGenresStr("мелодрама")
+            ),
+            7.258,
+            "2023-02-15",
+            true
+        ),
+        Premier(
+            20,
+            "https://kinopoiskapiunofficial.tech/images/posters/kp_small/5045951.jpg",
+            "О чем говорят мужчины. Простые удовольствия",
+            "",
+            listOf(
+                MockGenresStr("комедия")
+            ),
+            null,
+            "2023-02-15",
+            true
+        ),
+        Premier(
+            21,
+            "https://kinopoiskapiunofficial.tech/images/posters/kp_small/4673172.jpg",
+            "Хищники",
+            "As bestas",
+            listOf(
+                MockGenresStr("триллер"),
+                MockGenresStr("драма")
+            ),
+            7.258,
+            "2023-02-15",
+            false
+        ),
+        Premier(
+            22,
+            "https://kinopoiskapiunofficial.tech/images/posters/kp_small/8403.jpg",
+            "Небо над Берлином",
+            "Der Himmel über Berlin",
+            listOf(
+                MockGenresStr("фэнтези"),
+                MockGenresStr("драма"),
+                MockGenresStr("мелодрама")
+            ),
+            7.258,
+            "2023-02-15",
+            true
+        ),
+        Premier(
+            23,
+            "https://kinopoiskapiunofficial.tech/images/posters/kp_small/5045951.jpg",
+            "О чем говорят мужчины. Простые удовольствия",
+            "",
+            listOf(
+                MockGenresStr("комедия")
+            ),
+            null,
+            "2023-02-15",
+            true
+        ),
+        Premier(
+            24,
+            "https://kinopoiskapiunofficial.tech/images/posters/kp_small/4673172.jpg",
+            "Хищники",
+            "As bestas",
+            listOf(
+                MockGenresStr("триллер"),
+                MockGenresStr("драма")
+            ),
+            7.258,
+            "2023-02-15",
+            false
+        )
+    )
+
+    private class Premier(
+        override val id: Int,
+        override val posterUrlPreview: String?,
+        override val nameRu: String?,
+        override val nameEn: String?,
+        override val genres: List<GenreString>?,
+        override val rating: Double?,
+        override val premiereRu: String?,
+        override val isSeen: Boolean
+    ) : Movie
+
+    private class MockGenresStr(override val genre: String?) : GenreString
+}
--- data/src/main/java/com/home/cinema/data/repositories/page/home/HomePremiersRepository.kt
+++ data/src/main/java/com/home/cinema/data/repositories/page/home/HomePremiersRepository.kt
@@ -3,6 +3,7 @@ package com.home.cinema.data.repositories.page.home
 import com.home.cinema.data.exceptions.NetworkResponseException
 import com.home.cinema.data.network.retrofit.RetrofitInstance
 import com.home.cinema.data.network.retrofit.api.page.home.PremiersApi
+import com.home.cinema.data.repositories.mocks.page.home.MockPremiers
 import com.home.cinema.domain.models.entities.page.home.Movie
 import com.home.cinema.domain.models.params.page.home.GetHomePremiersParam
 import com.home.cinema.domain.repositories.page.home.PremiersRepository
@@ -16,5 +17,6 @@ class HomePremiersRepository : PremiersRepository {
         val response = loader.getPremiers(param.year, param.month)
         return response.body()?.items
             ?: throw NetworkResponseException("Code:${response.code()}, message: ${response.message()}")
+//        return MockPremiers().premiers
     }
 }
